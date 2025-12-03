be429f8d55582acddb249d34e71cc7e00c14b7a0
dsarisiswanto-pixel <dsarisiswanto@gmail.com>
2025-12-03 08:51:52 +0700

    Time: 1 ms (100.00%) | Memory: 20.3 MB (93.59%) - LeetSync

---
+++ 12-integer-to-roman/integer-to-roman.php
@@ -0,0 +1,33 @@
+class Solution {
+
+    /**
+     * @param Integer $num
+     * @return String
+     */
+    function intToRoman($num) {
+        $values = [
+            1000 => "M",
+            900 => "CM",
+            500 => "D",
+            400 => "CD",
+            100 => "C",
+            90 => "XC",
+            50 => "L",
+            40 => "XL",
+            10 => "X",
+            9 => "IX",
+            5 => "V",
+            4 => "IV",
+            1 => "I"
+        ];
+        $result = "" ;
+
+        foreach ($values as $value => $symbol) {
+            while ($num >= $value) {
+                $result .= $symbol;
+                $num -= $value;
+            }
+        }
+        return $result;
+    }
+}
