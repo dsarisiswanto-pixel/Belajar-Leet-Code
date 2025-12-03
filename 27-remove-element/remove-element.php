a1cb0dccf956cc599dbba733dcb2c5fbad694b92
dsarisiswanto-pixel <dsarisiswanto@gmail.com>
2025-12-03 11:18:43 +0700

    Time: 0 ms (100.00%) | Memory: 20.7 MB (18.26%) - LeetSync

---
+++ 27-remove-element/remove-element.php
@@ -0,0 +1,18 @@
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @param Integer $val
+     * @return Integer
+     */
+    function removeElement(&$nums, $val) {
+        $k = 0;
+        foreach ($nums as $num){
+            if ($num !== $val){
+                $nums[$k] = $num;
+                $k++;
+            }
+        }
+        return $k;
+    }
+}
