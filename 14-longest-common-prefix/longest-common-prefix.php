6719e5388520525b4d05e7e5e8805d05f177b2c3
dsarisiswanto-pixel <dsarisiswanto@gmail.com>
2025-12-03 08:56:12 +0700

    Time: 0 ms (100.00%) | Memory: 20.2 MB (93.21%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.php
@@ -0,0 +1,18 @@
+class Solution {
+
+    /**
+     * @param String[] 
+     * @return String
+     */
+    function longestCommonPrefix($strs) {
+        $prefix = $strs[0];
+        
+        for ($i = 1; $i < count($strs); $i++){
+            while (strpos($strs[$i], $prefix) !== 0) {
+                $prefix = substr($prefix, 0, -1);
+                if ($prefix === "") return "";
+            }
+        }
+        return $prefix;
+    }
+}
