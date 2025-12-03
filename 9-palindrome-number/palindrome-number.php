c0b600377843992a752c82632d81081511e3e88a
dsarisiswanto-pixel <dsarisiswanto@gmail.com>
2025-12-03 08:48:08 +0700

    Time: 3 ms (70.30%) | Memory: 20.7 MB (19.88%) - LeetSync

--- 9-palindrome-number/palindrome-number.php
+++ 9-palindrome-number/palindrome-number.php
@@ -8,7 +8,7 @@ class Solution {
         if ($x < 0) return false;
         if ($x % 10 == 0 && $x !=0) return false;
 
-        $rev = 0 ;
+        $rev = 0;
 
         while($x > $rev) {
             $rev = $rev * 10 + ($x % 10);
