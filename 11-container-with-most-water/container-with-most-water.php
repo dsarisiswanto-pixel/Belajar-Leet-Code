6e68230a53091a412bc2743269eb11acc75786ac
dsarisiswanto-pixel <dsarisiswanto@gmail.com>
2025-12-03 08:51:14 +0700

    Time: 48 ms (8.52%) | Memory: 26.8 MB (98.65%) - LeetSync

---
+++ 11-container-with-most-water/container-with-most-water.php
@@ -0,0 +1,25 @@
+class Solution {
+
+    /**
+     * @param Integer[] $height
+     * @return Integer
+     */
+    function maxArea($height) {
+        $l = 0;
+        $r = count($height) - 1;
+        $max = 0;
+
+        while ($l < $r) {
+            $area = ($r - $l) * min($height[$l], $height[$r]);
+            $max = max($max, $area);
+
+            if ($height[$l] < $height [$r]) {
+                $l++;
+            }
+            else {
+                $r--;
+            }
+        }
+        return $max;
+    }
+}
