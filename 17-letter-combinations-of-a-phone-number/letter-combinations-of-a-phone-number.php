d35c8f1901b74cf1a76d1a9761be7329b17ed907
dsarisiswanto-pixel <dsarisiswanto@gmail.com>
2025-12-03 09:05:39 +0700

    Time: 0 ms (100.00%) | Memory: 20.3 MB (88.24%) - LeetSync

---
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.php
@@ -0,0 +1,40 @@
+class Solution {
+
+    /**
+     * @param String 
+     * @return String[]
+     */
+    function letterCombinations($digits) {
+        if (empty($digits)) return [];
+
+        $map = [
+            '2' => 'abc', 
+            '3' => 'def', 
+            '4' => 'ghi', 
+            '5' => 'jkl', 
+            '6' => 'mno', 
+            '7' => 'pqrs', 
+            '8' => 'tuv', 
+            '9' => 'wxyz', 
+        ];
+
+        $result = [];
+
+     
+        $backtrack = function ($index, $path) use ($digits, $map, &$result, &$backtrack){
+            if ($index == strlen($digits)) {
+                $result[] = $path;
+                return;
+            }
+
+            $letters = $map[$digits[$index]];
+            for ($i = 0; $i < strlen($letters); $i++){
+                $backtrack($index + 1, $path . $letters[$i]);
+            }
+        };
+
+        $backtrack(0, "");
+
+        return $result;
+    }
+}
